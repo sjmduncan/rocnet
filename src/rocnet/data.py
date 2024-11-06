68141aa8c395c2018c4feab9f472d3a449a9cbc0
Stu <stu@sjmd.dev>
2024-11-06 16:40:50 +1300

    Construct octrees without having to allocate the whole occupancy grid beforehand

--- src/rocnet/data.py
+++ src/rocnet/data.py
@@ -158,10 +158,107 @@ def save(path, pts):
     o3d.io.write_point_cloud(path, pc2)
 
 
+def points_to_features(occupied_indices, grid_dim: int, leaf_dim: int):
+    """Encode a list of occupied indices to a list of node features/types, allocating memory only for occupied leaves rather than all grid_dim^3 voxels
+
+    occupied_indices: numpy array of indices of occupied voxels
+    grid_dim: edge length of the whole grid, if this is smaller than the bounds of the occupied indices then
+    leaf_dim: edge length of the octree leaf feature
+
+    returns: List of octrees, each in the form of a post-ordered list of node types and leaf features.
+             The number of octrees returned is determined by how many cubes of size grid_dim are required to contain the occupied indices
+    """
+    bl = np.min(occupied_indices[:, :3], axis=0)
+    tr = np.max(occupied_indices[:, :3], axis=0)
+
+    leaf_features = []
+
+    def get_feature_and_type(indices, origin, dim):
+        """Recursively compute node types, and node feature if relevant, at all octree levels"""
+        if dim < leaf_dim:
+            raise ValueError("sub_dim should always be larger than leaf_dim")
+        tr = origin + np.array([dim, dim, dim])
+        octant_indices = indices[np.all(list(zip(np.all(indices >= origin, axis=1), np.all(indices < tr, axis=1))), axis=1)]
+        if len(octant_indices) == 0:
+            leaf_features.append(torch.zeros([leaf_dim, leaf_dim, leaf_dim]))
+            return torch.tensor([Octree.NodeType.LEAF_EMPTY.value])
+        elif dim == leaf_dim:
+            occupancy = torch.zeros([leaf_dim, leaf_dim, leaf_dim])
+            local_indices = octant_indices - origin
+            occupancy[local_indices[:, 0], local_indices[:, 1], local_indices[:, 2]] = 1
+            leaf_features.append(occupancy)
+            return torch.tensor([Octree.NodeType.LEAF_MIX.value])
+        else:
+            octant_dim = dim // 2
+            octant_origin = [
+                np.array([0, 0, 0]),
+                np.array([octant_dim, 0, 0]),
+                np.array([0, octant_dim, 0]),
+                np.array([octant_dim, octant_dim, 0]),
+                np.array([0, 0, octant_dim]),
+                np.array([octant_dim, 0, octant_dim]),
+                np.array([0, octant_dim, octant_dim]),
+                np.array([octant_dim, octant_dim, octant_dim]),
+            ]
+            return torch.cat(
+                [
+                    get_feature_and_type(octant_indices, origin + octant_origin[0], octant_dim),
+                    get_feature_and_type(octant_indices, origin + octant_origin[1], octant_dim),
+                    get_feature_and_type(octant_indices, origin + octant_origin[2], octant_dim),
+                    get_feature_and_type(octant_indices, origin + octant_origin[3], octant_dim),
+                    get_feature_and_type(octant_indices, origin + octant_origin[4], octant_dim),
+                    get_feature_and_type(octant_indices, origin + octant_origin[5], octant_dim),
+                    get_feature_and_type(octant_indices, origin + octant_origin[6], octant_dim),
+                    get_feature_and_type(octant_indices, origin + octant_origin[7], octant_dim),
+                    torch.tensor([Octree.NodeType.NON_LEAF.value]),
+                ],
+                axis=0,
+            )
+
+    node_types = get_feature_and_type(occupied_indices, bl, grid_dim)
+    return torch.stack(leaf_features).type(torch.float32), node_types
+
+
+def features_to_points(leaf_features, node_types, grid_dim: int, leaf_dim: int):
+    """Decode a list of node features/types directly to a list of indices, allocating memory only for occupied leaves rather than all grid_dim^3 voxels"""
+
+    def sub_features_to_points(leaf_features, node_types, dim, origin):
+        print(node_types[-1], origin)
+        if node_types[-1] == Octree.NodeType.LEAF_EMPTY.value:
+            return np.array([]).reshape(-1, 3), node_types[:-1], leaf_features[:-1, :, :, :]
+        elif node_types[-1] == Octree.NodeType.LEAF_MIX.value:
+            pts = np.nonzero(leaf_features[-1]) + origin
+            return pts, node_types[:-1], leaf_features[:-1, :, :, :]
+        else:
+            octant_dim = dim // 2
+            octant_origin = [
+                np.array([0, 0, 0]),
+                np.array([octant_dim, 0, 0]),
+                np.array([0, octant_dim, 0]),
+                np.array([octant_dim, octant_dim, 0]),
+                np.array([0, 0, octant_dim]),
+                np.array([octant_dim, 0, octant_dim]),
+                np.array([0, octant_dim, octant_dim]),
+                np.array([octant_dim, octant_dim, octant_dim]),
+            ]
+            octant_origin.reverse()
+            pts1, node_types, leaf_features = sub_features_to_points(leaf_features, node_types[:-1], octant_dim, origin + octant_origin[0])
+            pts2, node_types, leaf_features = sub_features_to_points(leaf_features, node_types, octant_dim, origin + octant_origin[1])
+            pts3, node_types, leaf_features = sub_features_to_points(leaf_features, node_types, octant_dim, origin + octant_origin[2])
+            pts4, node_types, leaf_features = sub_features_to_points(leaf_features, node_types, octant_dim, origin + octant_origin[3])
+            pts5, node_types, leaf_features = sub_features_to_points(leaf_features, node_types, octant_dim, origin + octant_origin[4])
+            pts6, node_types, leaf_features = sub_features_to_points(leaf_features, node_types, octant_dim, origin + octant_origin[5])
+            pts7, node_types, leaf_features = sub_features_to_points(leaf_features, node_types, octant_dim, origin + octant_origin[6])
+            pts8, node_types, leaf_features = sub_features_to_points(leaf_features, node_types, octant_dim, origin + octant_origin[7])
+            pts = np.concatenate([pts1, pts2, pts3, pts4, pts5, pts6, pts7, pts8], axis=0)
+        return pts, node_types, leaf_features
+
+    occupied_indices, _, _ = sub_features_to_points(leaf_features, node_types, grid_dim, np.array([0,0,0]))
+    return occupied_indices
+
+
 def occupancy_to_features(occupancy_grid: torch.Tensor, leaf_size):
-    """
-    Return depth-first post-ordered list of node features and node types
-    """
+    """Return depth-first post-ordered list of node features and node types"""
     node_features = []
 
     def get_node_and_type(subgrid):
